918d5b31e020201ef04709cd3b05b67ae4959d27
Albert <albert@diskus.studio>
2019-04-08 18:06:53 +0200

    Complete getDogs function

--- src/actions/gameone/getDogs.js
+++ src/actions/gameone/getDogs.js
@@ -1,8 +1,9 @@
 import request from 'superagent'
+import { setDogs } from './setDogs'
 
 export function getDogs() {
     return function(dispatch) {
         request('https://dog.ceo/api/breeds/list/all')
-            .then(res => console.log(res))
+            .then(res => dispatch(setDogs(Object.keys(res.body.message))))
     }
 }
