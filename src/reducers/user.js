da83bd3f6174d81ec24b046459fa5e6441238019
gilian@kernkopje.net <gilian@kernkopje.net>
2019-04-10 18:19:24 +0200

    Implemented GuessesAnsweredCorrectly in reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,18 @@
 import { SET_USERNAME } from "../actions/user/setUserName";
 import { ANSWER_GIVEN } from "../actions/user/setUserScore";
 import { ANSWER_CORRECT } from "../actions/user/setUserScore";
-import { INCREMENT_CORRECT_GUESSES } from '../actions/user/incrementGuesses'
-import { RESET_CORRECT_GUESSES } from '../actions/user/resetGuesses'
+import { INCREMENT_CORRECT_GUESSES } from "../actions/user/incrementGuesses";
+import { RESET_CORRECT_GUESSES } from "../actions/user/resetGuesses";
 
-export default (state = { questionsAnswered: 0, score: 0, correctGuessesInARow: 0}, action = {}) => {
+export default (
+  state = {
+    questionsAnswered: 0,
+    questionsAnsweredCorrectly: 0,
+    score: 0,
+    correctGuessesInARow: 0
+  },
+  action = {}
+) => {
   switch (action.type) {
     case SET_USERNAME:
       return {
@@ -19,18 +27,19 @@ export default (state = { questionsAnswered: 0, score: 0, correctGuessesInARow: 
     case ANSWER_CORRECT:
       return {
         ...state,
-        score: state.score + 50
+        score: state.score + 50,
+        questionsAnsweredCorrectly: state.questionsAnsweredCorrectly + 1
       };
-      case INCREMENT_CORRECT_GUESSES:
+    case INCREMENT_CORRECT_GUESSES:
       return {
         ...state,
         correctGuessesInARow: state.correctGuessesInARow + 1
-      }
-      case RESET_CORRECT_GUESSES:
+      };
+    case RESET_CORRECT_GUESSES:
       return {
         ...state,
         correctGuessesInARow: 0
-      }
+      };
     default:
       return state;
   }
