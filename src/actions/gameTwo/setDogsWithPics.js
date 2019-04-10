84371a5f6cfc1a096960d25c78e0da721ab1d26d
EdwinHoenselaar <EdwinHoenselaar@gmail.com>
2019-04-10 17:56:09 +0200

    copied everything from game 1 so we dont interfere. Lots of duplicate code now

--- src/actions/gameTwo/setDogsWithPics.js
+++
@@ -1,8 +0,0 @@
-export const SET_DOGS_WITH_PICS = 'SET_DOGS_WITH_PICS'
-
-export function setDogsWithPics(array) {
-    return {
-        type: SET_DOGS_WITH_PICS,
-        payload: array,
-    }
-}
--- src/reducers/gameTwoDogs.js
+++ src/reducers/gameTwoDogs.js
@@ -1,10 +1,10 @@
-import { SET_DOGS_WITH_PICS } from '../actions/gameTwo/setDogsWithPics' 
+import { SET_DOGS_TWO } from '../actions/gameTwo/setDogsTwo' 
 
 const initialState = null
 
 export default (state = initialState, action = {}) => {
     switch (action.type) {
-    case SET_DOGS_WITH_PICS:
+    case SET_DOGS_TWO:
         return action.payload
     default:
         return state;
