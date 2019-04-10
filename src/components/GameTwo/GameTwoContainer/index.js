7345a795f3842bd9e6526936ae04e4e6b5f5522e
EdwinHoenselaar <EdwinHoenselaar@gmail.com>
2019-04-10 11:47:27 +0200

    cleaned up some code

--- src/components/GameTwo/GameTwoContainer/index.js
+++ src/components/GameTwo/GameTwoContainer/index.js
@@ -5,25 +5,31 @@ const DogListWithPictures = []
 
 export default class GameTwoContainer extends Component {
   componentDidMount = () => {
+    this.showPictures()
     request('https://dog.ceo/api/breeds/list/all')
       .then(response => {
           const DogList2 = Object.keys(response.body.message)
-          console.log(DogList2)
           return DogList2
       }).then(data => {
-          console.log("data: ",data)
           data.map(breed => {
               request(`https://dog.ceo/api/breed/${breed}/images/random/1`)
                 .then(response => {
-                    console.log("response 2: ",breed)
                     DogListWithPictures.push({[breed]: response.body.message[0]})
-                    console.log("Doglist with pictures?", DogListWithPictures)
                     return DogListWithPictures
                 })
           })
       })
   }
+
+  randomArray = () => {
+      const randomArray = [Math.floor(Math.random() * 87),Math.floor(Math.random() * 87),Math.floor(Math.random() * 87)]
+      return randomArray
+  }
   
+  showPictures = () => {
+      console.log("showpictures: ", DogListWithPictures[5])
+  }
+    
   render() {
       return (
           <div>
