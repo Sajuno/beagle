c96be9911323d86b743d497dd755cc029455f135
BenSchuddeboom <Benzel@live.nl>
2019-04-11 20:56:15 +0200

    intropage, playing with spring

--- src/components/IntroPage/index.js
+++ src/components/IntroPage/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import {Spring} from 'react-spring/renderprops'
+import './index.css'
 
 export default class index extends Component {
   render() {
-    return (
-      <div>
-        asd
-      </div>
+    return(
+    <Spring
+    config={{duration: 2000}}
+    from={{ opacity: 0 }}
+    to={{ opacity: 1 }}>
+    {props => <div style={props}><h1 className='intro-h1'>Henk de Vries presents...</h1></div>}
+    </Spring>
     )
   }
 }
