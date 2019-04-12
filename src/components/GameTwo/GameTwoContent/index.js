fb068ef4113f23b2b3d56c19ff82a761b5cf981b
EdwinHoenselaar <EdwinHoenselaar@gmail.com>
2019-04-12 11:17:13 +0200

    deleted standard bootstrap css and added custom css for game two

--- src/components/GameTwo/GameTwoContent/index.js
+++ src/components/GameTwo/GameTwoContent/index.js
@@ -16,6 +16,7 @@ import ModalHeader from 'react-bootstrap/ModalHeader'
 import ModalBody from 'react-bootstrap/ModalBody'
 import ModalFooter from 'react-bootstrap/ModalFooter'
 import Button from 'react-bootstrap/Button'
+import '../../../modal.css'
 
 class GameTwoContent extends Component {
     state = { show: false, answer: true }
